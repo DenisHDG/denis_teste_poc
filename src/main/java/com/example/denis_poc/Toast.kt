023ff74adaa451a7cbb798dda367b8934ad2d3d9
DENIS\denis <denishdg@gmail.com>
2023-05-22 21:29:37 -0300

    mudançao toaste

--- src/main/java/com/example/denis_poc/Toast.kt
+++ src/main/java/com/example/denis_poc/Toast.kt
@@ -3,7 +3,7 @@ package com.example.denis_poc
 import android.content.Context
 import android.widget.Toast
 
-class Toast {
+class Toaste {
     fun testeToast(context: Context){
         Toast.makeText(context, "testes banana", Toast.LENGTH_LONG).show()
     }
